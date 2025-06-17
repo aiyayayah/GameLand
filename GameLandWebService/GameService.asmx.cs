4ea73505791bdbf6ee988acb9e013b59960c5b80
Kah Yong <cky734@gmail.com>
2025-06-17 11:44:43 +0800

    setup webservice

---
+++ GameLandWebService/GameService.asmx.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Services;
+
+namespace GameLandWebService
+{
+    [WebService(Namespace = "http://gameland.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    public class GameService : WebService
+    {
+        [WebMethod]
+        public string Hello()
+        {
+            return "Hello from GameLand!";
+        }
+
+        [WebMethod]
+        public double CalculateCharge(double hours)
+        {
+            return hours * 5.00; // Example: RM5/hour
+        }
+    }
+}
