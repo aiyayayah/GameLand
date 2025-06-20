cdab09912a90372dca953137bd7ca6eb208121ef
Kah Yong <cky734@gmail.com>
2025-06-20 20:24:14 +0800

    update ui

--- GameLand/forms/UserDashboardForm.Designer.cs
+++ GameLand/forms/UserDashboardForm.Designer.cs
@@ -41,7 +41,7 @@
             // 
             this.lblWelcome.AutoSize = true;
             this.lblWelcome.Font = new System.Drawing.Font("Cambria", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWelcome.Location = new System.Drawing.Point(567, 43);
+            this.lblWelcome.Location = new System.Drawing.Point(758, 47);
             this.lblWelcome.Name = "lblWelcome";
             this.lblWelcome.Size = new System.Drawing.Size(103, 37);
             this.lblWelcome.TabIndex = 0;
@@ -52,17 +52,17 @@
             // 
             this.dgvAvailableItems.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvAvailableItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvAvailableItems.Location = new System.Drawing.Point(109, 121);
+            this.dgvAvailableItems.Location = new System.Drawing.Point(47, 121);
             this.dgvAvailableItems.Name = "dgvAvailableItems";
             this.dgvAvailableItems.RowHeadersWidth = 62;
             this.dgvAvailableItems.RowTemplate.Height = 28;
-            this.dgvAvailableItems.Size = new System.Drawing.Size(1328, 318);
+            this.dgvAvailableItems.Size = new System.Drawing.Size(1704, 494);
             this.dgvAvailableItems.TabIndex = 1;
             this.dgvAvailableItems.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvItems_CellContentClick);
             // 
             // btnBorrow
             // 
-            this.btnBorrow.Location = new System.Drawing.Point(738, 487);
+            this.btnBorrow.Location = new System.Drawing.Point(868, 621);
             this.btnBorrow.Name = "btnBorrow";
             this.btnBorrow.Size = new System.Drawing.Size(133, 51);
             this.btnBorrow.TabIndex = 2;
@@ -72,7 +72,7 @@
             // 
             // btnReturn
             // 
-            this.btnReturn.Location = new System.Drawing.Point(738, 755);
+            this.btnReturn.Location = new System.Drawing.Point(868, 903);
             this.btnReturn.Name = "btnReturn";
             this.btnReturn.Size = new System.Drawing.Size(133, 51);
             this.btnReturn.TabIndex = 3;
@@ -84,11 +84,11 @@
             // 
             this.dgvBorrowedItems.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvBorrowedItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvBorrowedItems.Location = new System.Drawing.Point(109, 568);
+            this.dgvBorrowedItems.Location = new System.Drawing.Point(47, 727);
             this.dgvBorrowedItems.Name = "dgvBorrowedItems";
             this.dgvBorrowedItems.RowHeadersWidth = 62;
             this.dgvBorrowedItems.RowTemplate.Height = 28;
-            this.dgvBorrowedItems.Size = new System.Drawing.Size(1328, 157);
+            this.dgvBorrowedItems.Size = new System.Drawing.Size(1704, 156);
             this.dgvBorrowedItems.TabIndex = 4;
             this.dgvBorrowedItems.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBorrowedItems_CellContentClick);
             // 
@@ -96,7 +96,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1495, 852);
+            this.ClientSize = new System.Drawing.Size(1797, 996);
             this.Controls.Add(this.dgvBorrowedItems);
             this.Controls.Add(this.btnReturn);
             this.Controls.Add(this.btnBorrow);
